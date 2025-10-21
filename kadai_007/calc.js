822f6df574cb50be6e5790c40809cbfc00fd0983
arc20a <tdtn4649@gmail.com>
2025-10-22 00:23:54 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+// calc.js
+
+// 1以上の正の数を代入
+let num = 15;
+
+// 3と5の倍数かどうかを判定
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+} 
+// 3の倍数かどうかを判定
+else if (num % 3 === 0) {
+    console.log("3の倍数です");
+} 
+// 5の倍数かどうかを判定
+else if (num % 5 === 0) {
+    console.log("5の倍数です");
+} 
+// それ以外の場合
+else {
+    console.log(num);
+}
